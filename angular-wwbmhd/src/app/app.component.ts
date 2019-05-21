79c31a8644c66f2c028ccd970687eb17f1aa996d
snjuguna <40172507+snjuguna@users.noreply.github.com>
2019-05-21 14:00:24 -0400

    Add files via upload

---
+++ angular-wwbmhd/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  preventDefault(){
+    event.preventDefault();
+  }
+  onClick(event:Event){
+    event.preventDefault();
+  }
+}
+
